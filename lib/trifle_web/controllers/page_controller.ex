1adc2eb7794a673ac81ecfef6b66a8c417396987
Jozef Vaclavik <jozef@hey.com>
2023-05-31 13:05:16 +0400

    feat: landing, navigation, toc & privacy

--- lib/trifle_web/controllers/page_controller.ex
+++ lib/trifle_web/controllers/page_controller.ex
@@ -1,9 +1,17 @@
 defmodule TrifleWeb.PageController do
   use TrifleWeb, :controller
 
+  plug :put_layout, [html: {TrifleWeb.Layouts, :page}]
+
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
     render(conn, :home, layout: false)
+  end
+
+  def toc(conn, _params) do
+    render(conn, :toc)
+  end
+
+  def privacy(conn, _params) do
+    render(conn, :privacy)
   end
 end
--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Trifle.MixProject do
   def application do
     [
       mod: {Trifle.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
@@ -36,6 +36,7 @@ defmodule Trifle.MixProject do
       {:phoenix, "~> 1.7.2"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
+      {:ecto_psql_extras, "~> 0.6"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
