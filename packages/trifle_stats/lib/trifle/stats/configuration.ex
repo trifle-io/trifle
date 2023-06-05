91aff50064b606384d7dd316baca75ebaa2d0bb4
Jozef Vaclavik <jozef@hey.com>
2023-06-05 22:50:43 +0400

    feat: Trifle.Stats

---
+++ packages/trifle_stats/lib/trifle/stats/configuration.ex
@@ -0,0 +1,14 @@
+defmodule Trifle.Stats.Configuration do
+  defstruct driver: nil, ranges: [:minute, :hour, :day, :week, :month, :quarter, :year], separator: "::", time_zone: "GMT", time_zone_database: nil, beginning_of_week: :monday
+
+  def configure(driver, track_ranges \\ [:minute, :hour, :day, :week, :month, :quarter, :year], separator \\ "::", time_zone \\ "GMT", time_zone_database \\ nil, beginning_of_week \\ :monday) do
+    %Trifle.Stats.Configuration{
+      driver: driver,
+      ranges: MapSet.intersection(MapSet.new(track_ranges), MapSet.new([:minute, :hour, :day, :week, :month, :quarter, :year])),
+      separator: separator,
+      time_zone: time_zone,
+      time_zone_database: time_zone_database,
+      beginning_of_week: beginning_of_week
+    }
+  end
+end
