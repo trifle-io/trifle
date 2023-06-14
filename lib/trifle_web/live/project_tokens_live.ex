4783b53ad6eabf17f523bee4d7c0894813d74923
Jozef Vaclavik <jozef@hey.com>
2023-06-14 07:05:16 +0400

    feat: move new forms to modals

--- lib/trifle_web/live/project_tokens_live.ex
+++ lib/trifle_web/live/project_tokens_live.ex
@@ -2,7 +2,6 @@ defmodule TrifleWeb.ProjectTokensLive do
   use TrifleWeb, :live_view
 
   alias Trifle.Organizations
-  alias Trifle.Organizations.Project
   alias Trifle.Organizations.ProjectToken
 
   def render(assigns) do
@@ -35,80 +34,143 @@ defmodule TrifleWeb.ProjectTokensLive do
     </div>
 
     <%= if @is_new do %>
-      <%= if @token do %>
-        <div class="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
-           <div class="px-4 py-5 sm:px-6">
-            <h2 class="text-bold">Token: <%= @token.name %></h2>
-          </div>
-          <div class="px-4 py-5 sm:p-6">
+      <div class="relative z-10" aria-labelledby="slide-over-title" role="dialog" aria-modal="true">
+        <!-- Background backdrop, show/hide based on slide-over state. -->
+        <div class="fixed inset-0"></div>
 
-            <div class="px-4 sm:px-0">
-              <h3 class="text-base font-semibold leading-7 text-gray-900">You've successfully created a Token!</h3>
-              <p class="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Please copy your token now as this is the last time you can access it.</p>
-            </div>
-            <div class="mt-6 border-t border-gray-100">
-              <dl class="divide-y divide-gray-100">
-                <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                  <dd class="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
-                    <span id="project_token_token" class="rounded-md font-mono text-lg px-2 w-auto max-w-max break-normal text-red-700 bg-red-100"><%= @token.token %></span>
+        <div class="fixed inset-0 overflow-hidden">
+          <div class="absolute inset-0 overflow-hidden">
+            <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16" x-transition:enter="transform transition ease-in-out duration-500 sm:duration-700" x-transition:enter-start="translate-x-full" x-transition:enter-end="translate-x-0" x-transition:leave="transform transition ease-in-out duration-500 sm:duration-700" x-transition:leave-start="translate-x-0" x-transition:leave-end="translate-x-full">
+              <div class="pointer-events-auto w-screen max-w-2xl">
+                <%= if @token do %>
+                  <div class="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                    <div class="flex-1">
+                      <!-- Header -->
+                      <div class="bg-gray-50 px-4 py-6 sm:px-6">
+                        <div class="flex items-start justify-between space-x-3">
+                          <div class="space-y-1">
+                            <h2 class="text-base font-semibold leading-6 text-gray-900" id="slide-over-title">New Token</h2>
+                            <p class="text-sm text-gray-500">You've successfully created a Token!</p>
+                          </div>
+                          <div class="text-gray-400 hover:text-gray-500">
+                            <.link navigate={~p"/app/projects/#{@project.id}/tokens"} class="">
+                              <span class="sr-only">Close panel</span>
+                              <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                              </svg>
+                            </.link>
+                          </div>
+                        </div>
+                      </div>
 
-                    <span phx-click={JS.dispatch("phx:copy", to: "#project_token_token")}>
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
-                      </svg>
-                      Copy
-                    </span>
-                  </dd>
-                </div>
-              </dl>
-            </div>
-          </div>
-          <div class="px-4 py-4 sm:px-6">
-            <.link navigate={~p"/app/projects/#{@project.id}/tokens"} class="">Done</.link>
-          </div>
-        </div>
-      <% else %>
-        <.form for={@form} phx-submit="create">
-          <div class="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
-            <div class="px-4 py-5 sm:px-6">
-              <h2 class="text-bold">New Token</h2>
-            </div>
-            <div class="px-4 py-5 sm:p-6">
+                      <!-- Container -->
+                      <div class="space-y-6 py-6 sm:space-y-0 sm:divide-y sm:divide-gray-200 sm:py-0">
+                        <div class="space-y-2 px-4 sm:grid sm:grid-cols-1 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
 
-              <div class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                <div class="sm:col-span-6">
-                  <label for="project_token_name" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
-                  <div class="mt-2">
-                    <.input field={@form[:name]} placeholder="" autocomplete="off" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                          <div class="border-l-4 border-yellow-400 bg-yellow-50 p-4">
+                            <div class="flex">
+                              <div class="flex-shrink-0">
+                                <svg class="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                  <path fill-rule="evenodd" d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+                                </svg>
+                              </div>
+                              <div class="ml-3">
+                                <p class="text-sm text-yellow-700">
+                                  Please copy your token now as this is the last time you can access it.
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+
+                          <span id="project_token_token" class="rounded-md font-mono text-lg px-2 w-auto max-w-max break-normal text-red-700 bg-red-100"><%= @token.token %></span>
+
+                          <span phx-click={JS.dispatch("phx:copy", to: "#project_token_token")}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline">
+                              <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
+                            </svg>
+                            Copy
+                          </span>
+                        </div>
+
+                      </div>
+                    </div>
+
+                    <!-- Action buttons -->
+                    <div class="flex-shrink-0 border-t border-gray-200 px-4 py-5 sm:px-6">
+                      <div class="flex justify-end space-x-3">
+                        <.link navigate={~p"/app/projects/#{@project.id}/tokens"} class="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Done</.link>
+                      </div>
+                    </div>
                   </div>
-                </div>
+                <% else %>
+                  <.form for={@form} phx-submit="create" class="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                    <div class="flex-1">
+                      <!-- Header -->
+                      <div class="bg-gray-50 px-4 py-6 sm:px-6">
+                        <div class="flex items-start justify-between space-x-3">
+                          <div class="space-y-1">
+                            <h2 class="text-base font-semibold leading-6 text-gray-900" id="slide-over-title">New Token</h2>
+                            <p class="text-sm text-gray-500">Create new Token to access project programatically. You know, to push some data into it.</p>
+                          </div>
+                          <div class="text-gray-400 hover:text-gray-500">
+                            <.link navigate={~p"/app/projects/#{@project.id}/tokens"} class="">
+                              <span class="sr-only">Close panel</span>
+                              <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                              </svg>
+                            </.link>
+                          </div>
+                        </div>
+                      </div>
 
-                <div class="sm:col-span-3">
-                  <label for="project_token_read" class="block text-sm font-medium leading-6 text-gray-900">Read</label>
-                  <div class="mt-2">
-                    <.input field={@form[:read]} type="checkbox" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                  </div>
-                </div>
+                      <!-- Container -->
+                      <div class="space-y-6 py-6 sm:space-y-0 sm:divide-y sm:divide-gray-200 sm:py-0">
+                        <div class="space-y-2 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
+                          <div>
+                            <label for="project_token_name" class="block text-sm font-medium leading-6 text-gray-900 sm:mt-1.5">Name</label>
+                          </div>
+                          <div class="sm:col-span-2">
+                            <.input field={@form[:name]} placeholder="" autocomplete="off" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                          </div>
+                        </div>
 
-                <div class="sm:col-span-3">
-                  <label for="project_token_write" class="block text-sm font-medium leading-6 text-gray-900">Write</label>
-                  <div class="mt-2">
-                    <.input field={@form[:write]} type="checkbox" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                  </div>
-                </div>
+                        <div class="space-y-2 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
+                          <div>
+                            <label for="project_token_read" class="block text-sm font-medium leading-6 text-gray-900 sm:mt-1.5">Read Access</label>
+                          </div>
+                          <div class="sm:col-span-2">
+                            <.input field={@form[:read]} type="checkbox" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                          </div>
+                        </div>
+
+                        <div class="space-y-2 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
+                          <div>
+                            <label for="project_token_write" class="block text-sm font-medium leading-6 text-gray-900 sm:mt-1.5">Write Access</label>
+                          </div>
+                          <div class="sm:col-span-2">
+                            <.input field={@form[:write]} type="checkbox" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <!-- Action buttons -->
+                    <div class="flex-shrink-0 border-t border-gray-200 px-4 py-5 sm:px-6">
+                      <div class="flex justify-end space-x-3">
+                        <.button phx-disable-with="Creating..." class="inline-flex justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create</.button>
+                        <.link navigate={~p"/app/projects/#{@project.id}/tokens"} class="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Cancel</.link>
+                      </div>
+                    </div>
+                  </.form>
+                <% end %>
               </div>
             </div>
-            <div class="px-4 py-4 sm:px-6">
-              <.button phx-disable-with="Creating...">Create</.button>
-              <.link navigate={~p"/app/projects/#{@project.id}/tokens"} class="">Cancel</.link>
-            </div>
           </div>
-        </.form>
-      <% end %>
-    <% else %>
-      <.link navigate={~p"/app/projects/#{@project.id}/tokens/new"} class="">New</.link>
+        </div>
+      </div>
     <% end %>
 
+    <.link navigate={~p"/app/projects/#{@project.id}/tokens/new"} class="">New</.link>
 
     <div class="overflow-hidden bg-white shadow sm:rounded-md mt-4">
       <ul role="list" class="divide-y divide-gray-200">
