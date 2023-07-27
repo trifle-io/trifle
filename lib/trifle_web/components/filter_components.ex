ea990e42764d24956cd8af70d7a1aef1d802792d
Jozef Vaclavik <jozef@hey.com>
2023-07-27 14:48:50 +0400

    feat: some project explore tweaks

--- lib/trifle_web/components/filter_components.ex
+++ lib/trifle_web/components/filter_components.ex
@@ -57,7 +57,7 @@ defmodule TrifleWeb.FilterComponents do
       <select
         id={@id}
         name={@name}
-        class="block w-full rounded-md border-0 py-1.5 bg-gray-100 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6"
+        class="block w-full rounded-md border-0 py-1.5 bg-white text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6"
         multiple={@multiple}
         {@rest}
       >
@@ -80,7 +80,7 @@ defmodule TrifleWeb.FilterComponents do
         id={@id}
         value={Phoenix.HTML.Form.normalize_value(@type, @value)}
         class={[
-          "block w-full rounded-md border-0 py-1.5 bg-gray-100 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6",
+          "block w-full rounded-md border-0 py-1.5 bg-white text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6",
           "phx-no-feedback:border-teal-300 phx-no-feedback:focus:border-teal-400",
           "focus:ring-2 focus:ring-inset focus:ring-teal-500",
           @errors != [] && "border-rose-400 focus:border-rose-400"
@@ -100,7 +100,7 @@ defmodule TrifleWeb.FilterComponents do
 
   def filter_label(assigns) do
     ~H"""
-    <label for={@for} class="absolute -top-2 left-2 inline-block bg-gray-100 px-1 text-xs font-medium text-gray-900">
+    <label for={@for} class="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900">
       <%= render_slot(@inner_block) %>
     </label>
     """
