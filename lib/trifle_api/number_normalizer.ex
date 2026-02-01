a328bcba9de5eee0f228367a8de25b3c69faf029
Jozef Vaclavik <jozef@hey.com>
2026-02-01 10:36:49 +0400

    feat: normalize API payloads

---
+++ lib/trifle_api/number_normalizer.ex
@@ -0,0 +1,18 @@
+defmodule TrifleApi.NumberNormalizer do
+  @moduledoc false
+
+  alias Decimal, as: D
+
+  def normalize(%D{} = decimal), do: D.to_float(decimal)
+  def normalize(%DateTime{} = dt), do: dt
+  def normalize(%NaiveDateTime{} = ndt), do: ndt
+
+  def normalize(%{} = map) when not is_struct(map) do
+    map
+    |> Enum.map(fn {key, value} -> {key, normalize(value)} end)
+    |> Map.new()
+  end
+
+  def normalize(list) when is_list(list), do: Enum.map(list, &normalize/1)
+  def normalize(other), do: other
+end
