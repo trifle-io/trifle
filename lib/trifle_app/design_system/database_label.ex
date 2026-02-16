936228540187c22e19b1c0fdcf8c7ac6e87d5abb
Jozef Vaclavik <jozef@hey.com>
2026-02-16 18:09:47 +0400

    fix: broken tests

--- lib/trifle_app/design_system/database_label.ex
+++ lib/trifle_app/design_system/database_label.ex
@@ -1,6 +1,15 @@
 defmodule TrifleApp.DesignSystem.DatabaseLabel do
   use Phoenix.Component
 
+  @doc false
+  def driver_display_name("mysql"), do: "MySQL"
+  def driver_display_name("postgres"), do: "Postgres"
+  def driver_display_name("mongo"), do: "MongoDB"
+  def driver_display_name("redis"), do: "Redis"
+  def driver_display_name("sqlite"), do: "SQLite"
+  def driver_display_name(driver) when is_binary(driver), do: String.capitalize(driver)
+  def driver_display_name(_driver), do: "Unknown"
+
   @doc """
   Renders a consistent database type label with blue styling.
 
@@ -11,15 +20,6 @@ defmodule TrifleApp.DesignSystem.DatabaseLabel do
   """
   attr :driver, :string, required: true
   attr :class, :string, default: ""
-
-  def driver_display_name("mysql"), do: "MySQL"
-  def driver_display_name("postgres"), do: "Postgres"
-  def driver_display_name("mongo"), do: "MongoDB"
-  def driver_display_name("redis"), do: "Redis"
-  def driver_display_name("sqlite"), do: "SQLite"
-  def driver_display_name(driver) when is_binary(driver), do: String.capitalize(driver)
-  def driver_display_name(_driver), do: "Unknown"
-
   def database_label(assigns) do
     ~H"""
     <span class={[
